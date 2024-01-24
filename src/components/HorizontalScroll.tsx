dff5794b97b5a4a7dab05dcddd01cdf4d79fb304
Jmpmen <mendozajamesmichael@gmail.com>
2024-01-24 15:06:02 +0800

    added large size

--- src/components/HorizontalScroll.tsx
+++ src/components/HorizontalScroll.tsx
@@ -2,13 +2,16 @@ import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { Card } from "./Card";
 import { IAnimeResult } from "@consumet/extensions/dist/models/types";
 import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
 
 export default function HorizontalScroll({
   title,
   animes,
+  large = false,
 }: {
   title: string;
   animes: IAnimeResult[];
+  large?: boolean;
 }) {
   return (
     <>
@@ -23,14 +26,15 @@ export default function HorizontalScroll({
         <ScrollArea>
           <div className="flex space-x-4 pb-4">
             {animes.map((anime) => (
-              <Card
-                key={anime.id}
-                anime={anime}
-                className="w-[150px]"
-                aspectRatio="square"
-                width={150}
-                height={150}
-              />
+              <Link href={"/" + anime.id} key={anime.id}>
+                <Card
+                  anime={anime}
+                  className={large ? "w-[200px]" : "w-[150px]"}
+                  aspectRatio={large ? "portrait" : "square"}
+                  width={large ? 200 : 150}
+                  height={large ? 200 : 150}
+                />
+              </Link>
             ))}
           </div>
           <ScrollBar orientation="horizontal" />
