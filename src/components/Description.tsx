f120c2d0b1d87f08fa00ee390d9113fd4ff0c892
Jmpmen <mendozajamesmichael@gmail.com>
2024-01-26 20:51:27 +0800

    switched to dark mode

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -2,7 +2,7 @@ import { IAnimeInfo } from "@consumet/extensions/dist/models/types";
 
 export default function Description({ anime }: { anime: IAnimeInfo }) {
   return (
-    <div className="bg-slate-800 min-h-48 rounded-b-lg text-slate-100 p-5 flex flex-col gap-1">
+    <div className="min-h-48 p-5 flex flex-col gap-1">
       <div>
         <span className="font-bold">Status: </span>
         <span className="text-sm text-slate-400">{anime.status}</span>
