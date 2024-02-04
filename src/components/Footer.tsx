77c38634f1bcaba7d69d2f4284d43e4ee80868b6
Jmpmen <mendozajamesmichael@gmail.com>
2024-02-04 19:20:16 +0800

    fixed footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="mt-5 w-full border-b border-border/40 bg-slate-500/95 backdrop-blur supports-[backdrop-filter]:bg-slate-500/60">
-      <div className="h-14 flex justify-center items-center w-full xl:max-w-4xl 2xl:max-w-6xl mx-auto text-xs font-thin font-mono ">
+    <footer className="mt-5 w-full h-14 border-b border-border/40 bg-slate-500/95 backdrop-blur supports-[backdrop-filter]:bg-slate-500/60 grid place-content-center">
+      <div className="text-center xl:max-w-4xl 2xl:max-w-6xl text-xs font-thin font-mono ">
         <span>
-          Copyright © {new Date().getFullYear()} - All rights reserved by
+          Copyright © {new Date().getFullYear()} - All rights reserved by{" "}
         </span>
         <Link
           href="https://jmpmen.netlify.app"
           target="_blank"
-          className="ml-1 italic hover:underline"
+          className="italic hover:underline text-nowrap"
         >
           James Michael Mendoza
         </Link>
